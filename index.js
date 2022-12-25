bfe313d92f5ab985c71c09dfcc10993d6c88efb0
Mykola Pavliv <mpavliv@ukr.net>
2022-12-25 13:51:22 +0200

    added

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const cors = require('cors');
+const port = 5000;
+//const HOST = '10.10.0.52';
+const HOST = 'localhost';
+let count = 1;
 
 app.use(express.json());
+app.use(cors());
 
 const Database = require('better-sqlite3');
 const e = require('express');
@@ -11,24 +16,41 @@ const db = new Database('temperature.db');
 let sql;
 
 sql = "DROP TABLE t_data";
-//db.exec(sql);
+//  db.exec(sql);
 
 sql = `CREATE TABLE IF NOT EXISTs t_data(point INTEGER, datatime INTEGER, value REAL)`;
 db.exec(sql);
 
 
 app.get('/', (req, res) => {
-  console.log('request');
+  console.log(' / request GET');
+  console.log('request', req);
   res.send('123');
 });
 
 app.get('/temperature', (req, res) => {
+  console.log(' /temperature request GET', req);
+  res.send('Hello World!');
+});
+
+app.get('/temperature/current', (req, res) => {
+  const row = db.prepare('SELECT MAX(datatime), value FROM t_data').get();
+  if (row) {
+    const currentTemperature = {"point": 1, "temperature": row.value};
+    res.status(200).send(JSON.stringify(currentTemperature));
+  }
+  else {
+    res.status(400).send("Error");
+  }
+  console.log(' /temperature request GET', req);
   res.send('Hello World!');
 });
 
 
+
 app.post('/temperature', (req, res) => {
-  console.log("req", req);
+  count = count + 1;
+  console.log(` /temperature request POST ${count}`);
   const rBody = req.body;
   console.log(rBody);
   if ("point" in rBody && "datatime" in rBody && "value" in rBody) {
@@ -37,7 +59,7 @@ app.post('/temperature', (req, res) => {
     console.log(point, datatime, value);
     const now = Date.now();
     console.log(now);
-    const resInsert = insert.run(point, datatime, value);
+    const resInsert = insert.run(point, now, value);
     console.log(resInsert);
     res.send('Inserted !');
   } else {
@@ -48,6 +70,6 @@ app.post('/temperature', (req, res) => {
 });
 
 
-app.listen(port, '10.10.0.52', () => {
-  console.log(`Example app listening at http://10.10.0.52:${port}`);
+app.listen(port, HOST, () => {
+  console.log(`Example app listening at http://${HOST}:${port}`);
 });
